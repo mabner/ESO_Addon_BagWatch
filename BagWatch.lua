12f0fbd2dfe8b3aa340b9e73be0e481efb4233e2
Marcos Leite <30819867+mabner@users.noreply.github.com>
2022-02-20 23:51:00 -0300

    idea code formatting

--- BagWatch.lua
+++ BagWatch.lua
@@ -2,17 +2,21 @@ BagWatch = {}
 
 BagWatch.name = "BagWatch"
 
-function BagWatch.Initialize()
+function BagWatch:Initialize()
+    self.inCombat = IsUnitInCombat("player")
     -- Create the BagWatch frame
 end
 
 -- Event handler
 function BagWatch.OnAddOnLoaded(event, addonName)
     if addonName == BagWatch.name then
-        BagWatch.Initialize()
+        BagWatch:Initialize()
     end
 end
 
+function OnBagUpdate(event, bagId)
+    -- Update the BagWatch frame
+end
 -- Registering events
 EVENT_MANAGER:RegisterForEvent(BagWatch.name, EVENT_ADD_ON_LOADED, BagWatch.OnAddOnLoaded)
 
