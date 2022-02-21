5397372306f85781566e39e9fcc593751a13c98e
Marcos Leite <30819867+mabner@users.noreply.github.com>
2022-02-21 02:58:20 -0300

    combat indicator displaying, not using saved variables

--- BagWatch.lua
+++ BagWatch.lua
@@ -2,15 +2,6 @@ BagWatch = {}
 
 BagWatch.name = "BagWatch"
 
-function BagWatch:Initialize()
-    self.inCombat = IsUnitInCombat("player")
-    self.bagSpace = GetNumBagFreeSlots(1)
-    -- Create the BagWatch frame
-    EVENT_MANAGER:RegisterForEvent(self.name, EVENT_PLAYER_COMBAT_STATE, self.OnPlayerCombatState)
-    self.savedVariables = ZO_SavedVars:NewAccountWide("BagWatchSavedVariables", 1, nil, {})
-
-end
-
 -- Event handler
 function BagWatch.OnAddOnLoaded(event, addonName)
     if addonName == BagWatch.name then
@@ -35,6 +26,35 @@ function BagWatch.OnPlayerCombatState(event, inCombat)
         --    d("Exiting combat.")
         --end
     end
+
+end
+
+function BagWatch.OnIndicatorMoveStop()
+    BagWatch.savedVariables.left = BagWatchIndicator:GetLeft()
+    BagWatch.savedVariables.top = BagWatchIndicator:GetTop()
+end
+
+function BagWatch:RestorePosition()
+    local left = self.savedVariables.left
+    local top = self.savedVariables.top
+
+    -- TODO: When using the saved position, the element is not displayed
+    --BagWatchIndicator:ClearAnchors()
+    --BagWatchIndicator:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, left, top)
+end
+
+function BagWatch:Initialize()
+    self.inCombat = IsUnitInCombat("player")
+    self.bagSpace = GetNumBagFreeSlots(1)
+
+    -- Create the BagWatch frame
+    EVENT_MANAGER:RegisterForEvent(self.name, EVENT_PLAYER_COMBAT_STATE, self.OnPlayerCombatState)
+
+    -- Account wide saved variable
+    self.savedVariables = ZO_SavedVars:NewAccountWide("BagWatchSavedVariables", 1, nil, {})
+
+    -- Restore saved position
+    self:RestorePosition()
 
 end
 
